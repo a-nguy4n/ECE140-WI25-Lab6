4b86458028ea8b1243da3b9267dd83396cd555c0
a-nguy4n <1512alli@gmail.com>
2025-02-17 18:50:57 -0800

    Working

--- IOT/src/main.cpp
+++ IOT/src/main.cpp
@@ -3,20 +3,78 @@
 
 // MQTT client - using descriptive client ID and topic
 #define CLIENT_ID "esp32-sensors"
-#define TOPIC_PREFIX "ENTER_SOMETHING_UNIQUE_HERE_THAT_SHOULD_ALSO_MATCH_MAINPY/ece140/sensors"
+#define TOPIC_PREFIX "alli/ece140/sensors"
 
 ECE140_MQTT mqtt(CLIENT_ID, TOPIC_PREFIX);
+ECE140_WIFI wifi;   // wifi object 
 
 // WiFi credentials
 const char* ucsdUsername = UCSD_USERNAME;
-const char* ucsdPassword = UCSD_PASSWORD;
+const char* ucsdPassword = "H0ng@n041512&N";
 const char* wifiSsid = WIFI_SSID;
 const char* nonEnterpriseWifiPassword = NON_ENTERPRISE_WIFI_PASSWORD;
 
+unsigned long lastPublish = 0; 
+
+// void mqttCallback(char* topic, uint8_t* payload, unsigned int length){
+//     Serial.print("Recieved message on topic: ");
+//     Serial.println(topic); 
+//     Serial.print("Message: ");
+//     for(unsigned int i = 0; i < length; i++){
+//         Serial.print((char)payload[i]);
+//     }
+//     Serial.println();
+// }
+
 void setup() {
     Serial.begin(115200);
+
+    // Connecting to Wifi here:
+    Serial.println("[Main] Connecting to Wifi...");
+    wifi.connectToWPAEnterprise(wifiSsid, ucsdUsername, ucsdPassword);
+    
+    // *** not UCSD wifi:  ***
+    // wifi.connectToWifi(wifiSsid, nonEnterpriseWifiPassword);
+
+    // To connect to the MQTT :
+    Serial.println("Connecting to MQTT broker...");
+    if (!mqtt.connectToBroker(1883)){
+        Serial.println("Failed to connect to MQTT broker");
+    }
+
+    // if(mqtt.connectToBroker()){
+    //     mqtt.publishMessage("status", "Sensor device connected!");
+    //     Serial.println("Connected to MQTT broker");
+    // }
+    // else{
+    //     Serial.println("Failed to connect to MQTT broker");
+    // }
 }
 
-void loop() {
+void loop(){
+    // Keep MQTT connection alive 
+    mqtt.loop();
 
+    // Read hall sensor 
+    int hallValue = hallRead(); 
+
+    // temp sensor 
+    float temperature = temperatureRead();
+
+    String message = "{\"hall_sensor\": " + String(hallValue) + ", \"temperature\": " + String(temperature) + "}";
+
+    // Serial.println("\n== Sensor Readings ===");
+    // Serial.print("Hall Sensor: ");
+    // Serial.println(hallValue);
+    // Serial.print("Temperature: ");
+    // Serial.print(temperature);
+    // Serial.println("C");
+    // Serial.println("==============");            
+    
+    Serial.println("Publishing Sensor Data...");
+    Serial.println(message);
+    mqtt.publishMessage("readings", message);
+    
+    delay(2000); // Waiting 2 seconds before next reading 
 }
+    
--- Server/main.py
+++ Server/main.py
@@ -1,18 +1,16 @@
 import paho.mqtt.client as mqtt
 import json
 from datetime import datetime
-from collections import deque
-import numpy as np
 
 # MQTT Broker settings
 BROKER = "broker.hivemq.com"
 PORT = 1883
-BASE_TOPIC = "ENTER_SOMETHING_UNIQUE_HERE_THAT_SHOULD_ALSO_MATCH_MAINCPP/ece140/sensors"
+BASE_TOPIC = "alli/ece140/sensors"
 TOPIC = BASE_TOPIC + "/#"
 
-if BASE_TOPIC == "ENTER_SOMETHING_UNIQUE_HERE_THAT_SHOULD_ALSO_MATCH_MAINCPP/ece140/sensors":
-    print("Please enter a unique topic for your server")
-    exit()
+# if BASE_TOPIC == "alli/ece140/sensors":
+#     print("Please enter a unique topic for your server")
+#     exit()
 
 
 def on_connect(client, userdata, flags, rc):
@@ -33,30 +31,39 @@ def on_message(client, userdata, msg):
         
         # check the topic if it is the base topic + /readings
         # if it is, print the payload
-            
+        # print(payload)
+        if msg.topic == (BASE_TOPIC + "/readings"):
+            print("Payload: " , payload)
+        
     except json.JSONDecodeError:
         print(f"\nReceived non-JSON message on {msg.topic}:")
         print(f"Payload: {msg.payload.decode()}")
 
 
-
 def main():
     # Create MQTT client
     print("Creating MQTT client...")
+    client = mqtt.Client()
 
     # Set the callback functions onConnect and onMessage
     print("Setting callback functions...")
+    client.on_connect = on_connect
+    client.on_message = on_message
     
     try:
         # Connect to broker
         print("Connecting to broker...")
+        client.connect(BROKER, PORT, 60) # 60 is timeout option
         
         # Start the MQTT loop
         print("Starting MQTT loop...")
+        client.loop_forever()            # press CTRL C to break loop 
         
     except KeyboardInterrupt:
         print("\nDisconnecting from broker...")
         # make sure to stop the loop and disconnect from the broker
+        client.loop_stop()
+        client.disconnect()
         print("Exited successfully")
     except Exception as e:
         print(f"Error: {e}")
